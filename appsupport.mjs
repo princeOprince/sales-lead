f7aaf7bf751dbb15e25a105bef460e3360ecda51
Prince Odia <princeodia@gmail.com>
2022-04-09 23:14:41 +0100

    Add appsupport module

---
+++ appsupport.mjs
@@ -0,0 +1,60 @@
+import createError from 'http-errors';
+import { port } from "./app.mjs";
+import { server } from "./app.mjs";
+import debug from "debug";
+const dbg = debug('notes:server');
+
+export function normalisePort(val) {
+    const port = parseInt(val);
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+export function onError(error) {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === "string"
+        ? "Pipe " + port : "Port " + port;
+
+    switch (error.code) {
+        case "EACCES":
+            console.error(`${bind} requires elevated privileges`);
+            process.exit(1);
+        case "EADDRINUSE":
+            console.error(`${bind} is already in use`);
+            process.exit(1);
+        default:
+            throw error;
+    }
+}
+
+export function onListening() {
+    const addr = server.address();
+    const bind = typeof addr === "string"
+        ? "pipe " + addr : "port " + addr.port;
+    dbg(`Listening on ${bind}`);
+}
+
+export function handle404(req, res, next) {
+    next(createError(404));
+}
+
+export function basicErrorHandler(err, req, res, next) {
+    //  defer to built-in error handler if headersSent
+    if (res.headersSent) {
+        return next(err);
+    }
+    //  set locals, only provideing error in development
+    res.local.message = err.message;
+    res.locals.error = req.app.get('env') === "development" ? err : {};
+    //  render the error page
+    res.status(err.status || 500);
+    res.render('error');
+}
