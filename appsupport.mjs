d3796a6240e801be3d86865ee6b1fca6a13af43b
Prince Odia <princeodia@gmail.com>
2022-04-10 11:55:49 +0100

    Fix undefined res.local object error

--- appsupport.mjs
+++ appsupport.mjs
@@ -52,7 +52,7 @@ export function basicErrorHandler(err, req, res, next) {
         return next(err);
     }
     //  set locals, only provideing error in development
-    res.local.message = err.message;
+    res.locals.message = err.message;
     res.locals.error = req.app.get('env') === "development" ? err : {};
     //  render the error page
     res.status(err.status || 500);
