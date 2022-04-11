2fd93e4200f623754669dca1633999559969bb95
Prince Odia <princeodia@gmail.com>
2022-04-11 21:58:06 +0100

    Add post route to submit email

--- controllers/index.mjs
+++ controllers/index.mjs
@@ -1,4 +1,10 @@
+import chalk from 'chalk';
 
 export const index = (req, res, next) => {
     res.render('index', { title: 'Express' });
+  }
+
+export const submitLead = (req, res, next) => {
+    console.log(chalk.bgRed('Lead Email:', req.body.lead_email));
+    res.redirect('/');
   }
--- routes/index.mjs
+++ routes/index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
-import { index } from '../controllers/index.mjs';
+import { index, submitLead } from '../controllers/index.mjs';
 export const router = express.Router();
 
 /* GET home page. */
 router.get('/', index);
+router.post('/', submitLead);
